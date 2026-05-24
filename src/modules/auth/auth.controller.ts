a00f58830ff750d9ca245eea943c29a24b60d531
devpolas <195308698+devpolas@users.noreply.github.com>
2026-05-24 08:44:57 +0600

    refactor auth functionality and clean

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,75 +6,77 @@ import {
   loginUserFromDb,
   sendJWT,
 } from "./auth.service.js";
+import { catchAsync } from "../../utils/catch_async.js";
+import { sendResponse } from "../../utils/send_response.js";
 
-export async function signup(req: Request, res: Response) {
-  try {
-    const body = req.body;
-    const { name, email, password, role } = body;
+export const signup = catchAsync(async (req: Request, res: Response) => {
+  const body = req.body;
+  const { name, email, password, role } = body;
 
-    if (!name || !email || !password || !role) {
-      return new AppError("Name, Email, Password and Role are required", 400);
-    }
-
-    const result = await createUserIntoDB({ name, email, password, role });
-
-    if (!result) {
-      return new AppError("Failed to create user", 500);
-    }
-
-    sendJWT(res, result, "access_token");
-
-    res.status(201).json({
-      success: true,
-      message: "User Created successfully!",
-      data: result,
-    });
-  } catch (error) {
-    console.error(error);
+  if (!name || !email || !password || !role) {
+    return new AppError("Name, Email, Password and Role are required", 400);
   }
-}
 
-export async function login(req: Request, res: Response) {
-  try {
-    const body = req.body;
-    const { email, password } = body;
+  const result = await createUserIntoDB({ name, email, password, role });
 
-    if (!email || !password) {
-      return new AppError("Email and Password are required", 400);
-    }
-
-    const result = await loginUserFromDb({ email, password });
-    if (!result) {
-      return new AppError("Invalid email or password", 401);
-    }
-    sendJWT(res, result, "access_token");
-
-    res.status(200).json({
-      success: true,
-      message: "User login successfully!",
-      data: result,
-    });
-  } catch (error) {
-    console.error(error);
+  if (!result) {
+    return new AppError("Failed to create user", 500);
   }
-}
 
-export async function refreshToken(req: Request, res: Response) {
-  try {
-    const result = await checkJWTToken(req.cookies.refresh_token);
+  sendJWT(res, result, "access_token");
+  sendJWT(res, result, "refresh_token");
 
-    if (!result) {
-      return new AppError("Unauthorized", 401);
-    }
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "User Created successfully!",
+    data: result,
+  });
+});
 
-    sendJWT(res, result, "access_token");
+export const login = catchAsync(async (req: Request, res: Response) => {
+  const body = req.body;
+  const { email, password } = body;
 
-    res.status(200).json({
-      success: true,
-      message: "Access token generated!",
-      data: result,
-    });
-  } catch (error) {
-    console.error(error);
+  if (!email || !password) {
+    return new AppError("Email and Password are required", 400);
   }
-}
+
+  const result = await loginUserFromDb({ email, password });
+  if (!result) {
+    return new AppError("Invalid email or password", 401);
+  }
+  sendJWT(res, result, "access_token");
+  sendJWT(res, result, "refresh_token");
+
+  res.status(200).json({
+    success: true,
+    message: "User login successfully!",
+    data: result,
+  });
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "User login successfully!",
+    data: result,
+  });
+});
+
+export const refreshToken = catchAsync(async (req: Request, res: Response) => {
+  const result = await checkJWTToken(req.cookies.refresh_token);
+
+  if (!result) {
+    return new AppError("Unauthorized", 401);
+  }
+
+  sendJWT(res, result, "access_token");
+  sendJWT(res, result, "refresh_token");
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Access token generated!",
+    data: result,
+  });
+});
